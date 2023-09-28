cc310c75b01b3bae6e1e0bdc3ea6427d5236bd09
Cloud User <ec2-user@ip-172-31-34-92.ec2.internal>
2023-09-28 02:09:57 +0000

    arrumando

--- proj1/main.c
+++ proj1/main.c
@@ -5,7 +5,7 @@
 #define FIBER_STACK 1024 * 64
 
 pthread_mutex_t mutex;
-int erro_impresso = 0; 
+int erro_impresso = 0;
 
 struct c {
   float saldo;
@@ -15,15 +15,15 @@ typedef struct c conta;
 void *transferencia(void *arg) {
   conta **contasArray = (conta **)arg;
 
-  float quantia = (*contasArray[2]).saldo;
+  float valor = (*contasArray[2]).saldo;
 
   pthread_mutex_lock(&mutex);
 
-  if ((*contasArray[0]).saldo >= quantia) {
-    (*contasArray[0]).saldo -= quantia;
-    (*contasArray[1]).saldo += quantia;
+  if ((*contasArray[0]).saldo >= valor) {
+    (*contasArray[0]).saldo -= valor;
+    (*contasArray[1]).saldo += valor;
   } else {
-    if (!erro_impresso) { 
+    if (!erro_impresso) {
       printf("Erro: Saldo insuficiente na conta from\n");
       erro_impresso = 1;
     }
@@ -32,51 +32,52 @@ void *transferencia(void *arg) {
   pthread_mutex_unlock(&mutex);
 
   return NULL;
+
 }
 
-int main(int argc, char *argv[]) {
-  int num;
-  float quantia;
-
+int main() {
+  int qtde;
+  float valor;
+  
   printf("Digite a quantidade de transações: ");
-  if (scanf("%d", &num) != 1) {
+  if (scanf("%d", &qtde) != 1) {
     printf("Erro ao ler a quantidade das transações.\n");
     return 1;
   }
-
   printf("Digite o valor das transações: ");
-  if (scanf("%f", &quantia) != 1) {
+  if (scanf("%f", &valor) != 1) {
     printf("Erro ao ler o valor das transações.\n");
     return 1;
   }
 
-  conta from, to, quantias;
+  conta from, to, valores;
   from.saldo = 100;
   to.saldo = 100;
-  quantias.saldo = quantia;
+  valores.saldo = valor;
 
   conta **arg = (conta **)malloc(3 * sizeof(conta *));
 
   arg[0] = &from;
   arg[1] = &to;
-  arg[2] = &quantias;
+  arg[2] = &valores;
 
-  pthread_t *thread_array = malloc(num * sizeof(pthread_t));
   if (pthread_mutex_init(&mutex, NULL) != 0) {
     printf("O mutex falhou!");
     return 1;
   }
 
-  for (int i = 0; i < num; i++) {
+  pthread_t *thread_array = malloc(qtde * sizeof(pthread_t));
+  
+  for (int i = 0; i < qtde; i++) {
     pthread_create(&thread_array[i], NULL, transferencia, (void *)arg);
   }
 
-  for (int i = 0; i < num; i++) {
+  for (int i = 0; i < qtde; i++) {
     pthread_join(thread_array[i], NULL);
   }
 
   if (!erro_impresso) {
-    printf("Transferindo %.2f para a conta to\n\n", quantia);
+    printf("Transferindo %.2f para a conta to\n\n", valor);
     printf("Conta from: %.2f\n", from.saldo);
     printf("Conta to: %.2f\n\n", to.saldo);
     printf("Transferência concluída com sucesso!\n");
@@ -84,8 +85,8 @@ int main(int argc, char *argv[]) {
 
   pthread_mutex_destroy(&mutex);
   free(thread_array);
+
   free(arg);
 
   return 0;
 }
-
